3e6c80e2199358bae076534d807b33f04b18d52b
Vidhan <vidh.bhatt@gmail.com>
2020-05-12 19:54:22 -0400

    update md_init to create files with template

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from datetime import date, timedelta, datetime
-import os, argparse
+import os, argparse, subprocess
 
 
 the_cal = {"t": 0, "tm": 1, "y": -1}
@@ -12,22 +12,39 @@ def read_args():  # Read commandline arguments and finalize the date
     aparser.add_argument(
         "-d",
         type=valid_date,
-        help="Date for the entry, t=today,y=yesterday,tm=tomorrow, or date in YYY/MM/DD format",
+        help="Date for the entry, t = today, y = yesterday, tm = tomorrow, or date in YYY/MM/DD format",
     )
-    date_string = aparser.parse_args().d
-    return date_string
+    arg_datestr = aparser.parse_args().d
+    return arg_datestr
 
 
-def open_note(note_file):  # Open md file using vim
+def md_open(open_md):  # Open md file using vim
     print("opening file")
-    os.system("vi" + " " + note_file)
+    os.system("vi" + " " + open_md)
 
 
-def md_init(date_label):  # Initialize md file with template and original content
-    the_file = date_label + ".md"
-    the_path = date_label[:7]
-    os.makedirs(the_path, exist_ok=True)
-    return the_file
+def md_init(init_date):
+    md_datehf = (
+        datetime.strftime(
+            datetime.strptime(init_date, "%Y/%m/%d"), "%B %d, %Y"
+        )  # converts YYYY/MM/DD to Month DD, YYYY
+        .lstrip("0")  # strips zero-padding from the day
+        .replace(" 0", " ")
+    )
+    init_rootdir = "journal/"
+    init_datedir = init_rootdir + init_date[:7]
+    init_datefile = init_rootdir + init_date + ".md"
+    os.makedirs(init_datedir, exist_ok=True)
+    f = open(init_datefile, "w+")
+    f.write("# " + md_datehf + "\n")
+    f.write("\n")
+    f.write("## To-Do\n")
+    f.write("\n")
+    f.write("## Notes\n")
+    f.write("\n")
+    f.write("## Journal\n")
+    f.close()
+    return init_datefile
 
 
 def valid_date(s):  # Helper function used by argparser to do date validation
@@ -50,4 +67,4 @@ def valid_date(s):  # Helper function used by argparser to do date validation
 
 date_label = read_args()  # Get date
 md_file = md_init(date_label)  # Get file
-open_note(md_file)  # Open the file
+md_open(md_file)  # Open the file
