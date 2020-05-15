be14ced29df99b4e603ae12cac7dc2a02902d66c
Manish <manish.bhatt@gmail.com>
2020-05-14 22:18:32 -0400

    wip

--- main.py
+++ main.py
@@ -5,17 +5,26 @@ from os import path
 
 
 the_cal = {"t": 0, "tm": 1, "y": -1}
-todo_tag="## To-do"
-notes_tag="## Notes"
-journal_tag="## Journal"
+todo_tag = "## To-do"
+notes_tag = "## Notes"
+journal_tag = "## Journal"
 the_md_file = {
     "uncategorized": [],
-    "header_order": ["Title",todo_tag,notes_tag,journal_tag],
+    "header_order": ["Title", todo_tag, notes_tag, journal_tag],
     "Title": "",
-    todo_tag: [todo_tag+"\n\n"],
-    notes_tag: [notes_tag+"\n\n"],
-    journal_tag: [journal_tag+"\n\n"]
+    todo_tag: [todo_tag + "\n\n", "- [ ]\n\n"],
+    notes_tag: [notes_tag + "\n\n"],
+    journal_tag: [journal_tag + "\n\n"],
 }
+
+
+def main():
+    # Main workflow
+    date = read_args()[0]  # Get date
+    the_file_name=md_init(date)  # Get filename
+    # Read the file, this function will be useful when we want to inject and update todos , notes etc..
+    md_read(the_file_name)  
+    md_open(the_file_name)  # Open the file
 
 
 def read_args():  # Read commandline arguments and finalize the date
@@ -49,12 +58,12 @@ def valid_date(s):  # Helper function used by argparser to do date validation
             datetime.now() + timedelta(days=time_delta), "%Y/%m/%d"
         )
 
-    try:
-        datetime.strptime(s, "%Y/%m/%d")
-        # Check if it's valid date or not by trying to convert into actual date
-        return datetime.strftime(datetime.strptime(s, "%Y/%m/%d"), "%Y/%m/%d")
-    except ValueError:
-        msg = "Not a valid date: '{0}'.".format(s)
+        try:
+            datetime.strptime(s, "%Y/%m/%d")
+            # Check if it's valid date or not by trying to convert into actual date
+            return datetime.strftime(datetime.strptime(s, "%Y/%m/%d"), "%Y/%m/%d")
+        except ValueError:
+            msg = "Not a valid date: '{0}'.".format(s)
         raise argparse.ArgumentTypeError(msg)
 
 
@@ -68,52 +77,47 @@ def md_init(init_date):
     )
     init_rootdir = "journal/"
     init_datedir = init_rootdir + init_date[:7]
-    init_datefile = init_rootdir + init_date + ".md"
+    md_file_name = init_rootdir + init_date + ".md"
     os.makedirs(init_datedir, exist_ok=True)
-    if not path.exists(init_datefile):
-        the_md_file["Title"] = md_datehf+"\n"*2
-        md_write(init_datefile)
-    return init_datefile
+    if not path.exists(md_file_name):
+        the_md_file["Title"] = md_datehf + "\n" * 2
+        md_write(md_file_name)
+    return md_file_name
 
-
-def md_read(read_path):
-    f = open(read_path, "r")
-    ar=[]
-    file_content=f.readlines()
-    the_md_file["Title"]=file_content[0]
+def md_read(md_file_name):
+    f = open(md_file_name, "r")
+    ar = []
+    file_content = f.readlines()
+    the_md_file["Title"] = file_content[0]
     for ln in file_content[1:]:
         if ln.strip() == todo_tag:
-            ar=the_md_file[todo_tag]
+            ar = the_md_file[todo_tag]
             continue
         if ln.strip() == notes_tag:
-            ar=the_md_file[notes_tag]
+            ar = the_md_file[notes_tag]
             continue
         if ln.strip() == journal_tag:
-            ar=the_md_file[journal_tag]
+            ar = the_md_file[journal_tag]
             continue
         ar.append(ln)
     pprint(the_md_file)
     f.close()
 
-def md_write(write_path):
-    f = open(write_path, "w+")
+
+def md_write(md_file_name):
+    f = open(md_file_name, "w+")
     for file_header in the_md_file["header_order"]:
         for header_contents in the_md_file[file_header]:
             for each_line in header_contents:
                 f.write(each_line)
 
-
     f.close()
 
 
-def md_open(open_md):  # Open md file using vim
+def md_open(md_file_name):  # Open md file using vim
     print("opening file")
-    os.system("vi " + open_md)
+    os.system("vi " + md_file_name)
 
 
-# Main workflow
-
-date = read_args()[0]  # Get date
-md_file = md_init(date)  # Get file
-md_read(md_file)  # Read the file, this function will be useful when we want to inject and update todos , notes etc..
-md_open(md_file)  # Open the file
+if __name__ == "__main__":
+    main()
